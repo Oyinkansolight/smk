8c61983b4f8239c59e7727b503905d7e28a13dfc
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2023-03-28 08:34:18 +0100

    feat: refactor cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,19 +3,15 @@
 import { CountCard } from '@/components/cards';
 
 const Page = () => {
-    return (
-        <div className='flex flex-col px-4 pt-6'>
-            <div className='flex flex-wrap gap-[27px]'>
-                <CountCard
-                    count={12566}
-                    title='Total Students'
-                    variant='primary'
-                />
-                <CountCard count={66} title='Total Staff' variant='secondary' />
-                <CountCard count={12} title='Total Grades' variant='tertiary' />
-            </div>
-        </div>
-    );
+  return (
+    <div className='flex flex-col px-4 pt-6'>
+      <div className='flex flex-wrap gap-3 md:gap-[20px] xl:gap-[27px]'>
+        <CountCard count={12566} title='Total Students' variant='primary' />
+        <CountCard count={66} title='Total Staff' variant='secondary' />
+        <CountCard count={12} title='Total Grades' variant='tertiary' />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/components/cards/Count.tsx
+++ src/components/cards/Count.tsx
@@ -7,37 +7,33 @@ import BasicCard from '@/components/cards/Basic';
 import UnstyledLink from '@/components/links/UnstyledLink';
 
 interface CountCardProps {
-    count: number;
-    title: string;
-    variant: 'primary' | 'secondary' | 'tertiary';
+  count: number;
+  title: string;
+  variant: 'primary' | 'secondary' | 'tertiary';
 }
 
 const CountCard = ({ count, title, variant }: CountCardProps) => {
-    return (
-        <BasicCard
-            className={clsxm(
-                variant === 'primary' && 'bg-[#FFEFEA] text-[#FF6633]',
-                variant === 'secondary' && 'bg-[#F8E6FF] text-[#660195]',
-                variant === 'tertiary' && 'bg-[#E3FFF5] text-[#02A369]',
-                '!m-0 flex h-[156px] w-full min-w-[180px] items-center whitespace-nowrap !rounded-[9px] px-[36px] py-[21px] shadow-sm lg:w-[230px]'
-            )}
-        >
-            <div className='flex flex-col gap-8'>
-                <div className='flex flex-col gap-2'>
-                    <div className='text-[21px] font-bold'>
-                        {commaNumber(count)}
-                    </div>
-                    <div className='text-4 font-semibold capitalize'>
-                        {title}
-                    </div>
-                </div>
+  return (
+    <BasicCard
+      className={clsxm(
+        variant === 'primary' && 'bg-[#FFEFEA] text-[#FF6633]',
+        variant === 'secondary' && 'bg-[#F8E6FF] text-[#660195]',
+        variant === 'tertiary' && 'bg-[#E3FFF5] text-[#02A369]',
+        '!m-0 flex h-[156px] w-full min-w-[180px] items-center whitespace-nowrap !rounded-[9px] px-[36px] py-[21px] shadow-sm lg:w-[180px] xl:w-[230px]'
+      )}
+    >
+      <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-2'>
+          <div className='text-[21px] font-bold'>{commaNumber(count)}</div>
+          <div className='text-4 font-semibold capitalize'>{title}</div>
+        </div>
 
-                <UnstyledLink href='#' className='text-[14px]'>
-                    Manage All
-                </UnstyledLink>
-            </div>
-        </BasicCard>
-    );
+        <UnstyledLink href='#' className='text-[14px]'>
+          Manage All
+        </UnstyledLink>
+      </div>
+    </BasicCard>
+  );
 };
 
 export default CountCard;
