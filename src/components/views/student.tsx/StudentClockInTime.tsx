9d34923db44b2d17fb9ba57fa884425535ade667
Abdullahi Abass <abassabdullahi4@gmail.com>
2023-04-03 07:10:27 +0100

    feat: update student dashboard

--- src/components/views/student.tsx/StudentClockInTime.tsx
+++ src/components/views/student.tsx/StudentClockInTime.tsx
@@ -3,8 +3,8 @@ import WeekCheckBox from '@/components/calendars/WeekCheckBox';
 
 export default function StudentClockInTime() {
   return (
-    <div className='flex flex-col items-center rounded-md border-blue1 bg-[#F6F8FF]'>
-      <div>
+    <div className='flex flex-col items-center gap-4 rounded-md border border-dodger-500 bg-[#F6F8FF] p-6'>
+      <div className='text-base'>
         You are <span className='text-red-500'>within</span> Your clock in area
       </div>
       <div className='relative aspect-square h-[100px] rounded-full bg-[#EBEBEB] shadow-md'>
@@ -13,7 +13,7 @@ export default function StudentClockInTime() {
         </div>
       </div>
       <Button>Clock In</Button>
-      <WeekCheckBox date={new Date()} />
+      <WeekCheckBox date={new Date('2023-04-05T05:27:14.617Z')} />
     </div>
   );
 }
