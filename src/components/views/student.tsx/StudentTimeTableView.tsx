a32c993365378fbc625f2eefb47fde90d85c4bf0
Abdullahi Abass <abassabdullahi4@gmail.com>
2023-04-04 07:47:30 +0100

    feat: update student dashboard

---
+++ src/components/views/student.tsx/StudentTimeTableView.tsx
@@ -0,0 +1,68 @@
+import moment from 'moment';
+import Calendar from 'react-widgets/Calendar';
+
+import 'react-widgets/styles.css';
+import '../../../styles/student-calendar.css';
+
+import clsxm from '@/lib/clsxm';
+
+import { CalendarStepper } from '@/components/calendars';
+const timeLineData = [
+  {
+    image: '/images/teacher_step_1.png',
+    title: 'MR. Gbadamosi’s Class',
+    details: '1:32 AM',
+  },
+  {
+    image: '/images/teacher_step_2.png',
+    title: 'Mrs. Erhveba’s Class',
+    details: '1:32 AM',
+  },
+  {
+    image: '/images/teacher_step_3.png',
+    title: 'Submitted a bug',
+    details: 'Yesterday 12:39 AM',
+  },
+  {
+    image: '/images/teacher_step_4.png',
+    title: 'Modified A data in Page X',
+    details: 'Aug 11',
+  },
+  {
+    image: '/images/teacher_step_2.png',
+    title: 'Mrs. Erhveba’s Class',
+    details: '3:30 PM',
+  },
+];
+export default function StudentTimeTableView() {
+  return (
+    <div className='flex items-start justify-between gap-10 py-8'>
+      <div className='max-w-xs'>
+        <Calendar className='border-fun-green' />
+      </div>
+      <div className='flex-1 rounded-lg bg-white p-4'>
+        <div className='flex items-center text-xl'>
+          <div className='font-bold text-[#6B7A99]'>Calender Items</div>
+          <div className='w-10' />
+          <div className='text-fun-green-500'>
+            ({moment().format('D-M-yyyy')})
+          </div>
+          <div className='flex-1' />
+          <button
+            className={clsxm(
+              'flex items-center justify-center',
+              'hover:bg-fun-green-500 hover:text-white',
+              'h-9 w-[142px] rounded border py-2 px-6 shadow-sm',
+              'active:bg-fun-green-500 active:text-white active:ring-1',
+              'whitespace-nowrap text-[10.8px] font-bold text-fun-green-500'
+            )}
+          >
+            Add Item To Calender
+          </button>
+        </div>
+        <div className='h-16' />
+        <CalendarStepper timeLine={timeLineData} />
+      </div>
+    </div>
+  );
+}
