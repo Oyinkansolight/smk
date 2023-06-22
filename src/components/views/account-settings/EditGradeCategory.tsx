2a6feec4f401245a6c23cf4a25260979d264519c
Abdullahi Abass <abassabdullahi4@gmail.com>
2023-06-22 12:41:25 +0100

    feat: updates

---
+++ src/components/views/account-settings/EditGradeCategory.tsx
@@ -0,0 +1,133 @@
+import Button from '@/components/buttons/Button';
+import Input from '@/components/input/formInput';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { BsTrashFill } from 'react-icons/bs';
+
+export default function EditGradeCategory({ isEdit }: { isEdit?: boolean }) {
+  const [items, setItems] = useState<
+    { category: string; percentage: number }[]
+  >([
+    {
+      category: 'CA 1',
+      percentage: 60,
+    },
+    {
+      category: 'CA 2',
+      percentage: 60,
+    },
+    {
+      category: 'Examination',
+      percentage: 60,
+    },
+  ]);
+  const [isEditing, setIsEditing] = useState(isEdit ?? false);
+  const { handleSubmit, setValue, control } = useForm({
+    reValidateMode: 'onChange',
+    mode: 'all',
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (data: any) => {
+    const newI = [...items];
+    for (let i = 0; i < items.length; i++) {
+      newI[i] = {
+        category: data[`category.${i}`],
+        percentage: data[`percentage.${i}`],
+      };
+    }
+    setItems(newI);
+  };
+
+  const handleAddNewCategory = () => {
+    setItems([...items, { category: '', percentage: 0 }]);
+  };
+
+  useEffect(() => {
+    for (let i = 0; i < items.length; i++) {
+      const it = items[i];
+      setValue(`category.${i}`, it.category);
+      setValue(`percentage.${i}`, it.percentage, { shouldDirty: true });
+    }
+  }, [items, setValue]);
+
+  return !isEditing ? (
+    <div>
+      <div className='bg-[#F4F9FF] font-bold p-4'>
+        <div className='text-xl my-4'>Institution Type</div>
+        <div className='text-center border-y py-5'>Primary</div>
+      </div>
+      <div className='h-4' />
+      <div className='flex flex-col gap-4 '>
+        {items.map((v, i) => (
+          <div key={i} className='flex p-4 border items-start rounded'>
+            <div className='flex-1 '>
+              <div className='text-[#A5A5A5]'>Category Name</div>
+              <div>{v.category}</div>
+            </div>
+            <div className='flex-1'>
+              <div className='text-[#A5A5A5]'>Percentage</div>
+              <div>{v.category}%</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='flex items-center justify-center mt-4'>
+        <Button
+          className='px-28'
+          onClick={() => setIsEditing(true)}
+          variant='outline'
+        >
+          Edit
+        </Button>
+      </div>
+    </div>
+  ) : (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='text-center'>
+        <div className='font-bold text-3xl'>Edit Grade Category</div>
+        <div>Kindly select the appropriate options below:</div>
+      </div>
+      <div>
+        {items.map((v, i) => (
+          <div key={i} className='flex items-end gap-3'>
+            <Controller
+              name={`category.${i}`}
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input label='Category Name' placeholder='' {...field} />
+                );
+              }}
+            />
+            <Controller
+              name={`percentage.${i}`}
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input
+                    label='Enter percentage score'
+                    placeholder=''
+                    {...field}
+                  />
+                );
+              }}
+            />
+            <div className='bg-[#FFF8F8] text-red-500 p-4 rounded-full'>
+              <BsTrashFill />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='h-4' />
+      <div className='flex'>
+        <Button variant='outline' onClick={handleAddNewCategory}>
+          Add New Grade Category
+        </Button>
+      </div>
+      <div className='flex w-full justify-center'>
+        <Button type='submit'>Submit</Button>
+      </div>
+    </form>
+  );
+}
--- src/components/views/account-settings/GradeBookSettings.tsx
+++ src/components/views/account-settings/GradeBookSettings.tsx
@@ -1,19 +1,58 @@
 import Button from '@/components/buttons/Button';
+import GeneralModal from '@/components/modals/general-modal';
+import EditGradeCategory from '@/components/views/account-settings/EditGradeCategory';
+import { useState } from 'react';
 import ReactTable, { TableColumn } from 'react-data-table-component';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const columns: TableColumn<any>[] = [
   { name: 'Institution Type', cell: (row) => row.type },
-  { name: 'Grade Category', cell: (row) => row.category },
+  {
+    name: 'Grade Category',
+    cell: (row) =>
+      (row.categories as string[]).length === 0
+        ? '-'
+        : (row.categories as string[]).join(','),
+  },
   {
     name: '',
     cell: (row) => (
-      <div className='flex justify-end w-full gap-4'>{row.actions}</div>
+      <div className='flex justify-end w-full gap-4'>
+        {(row.categories as string[]).length === 0 ? (
+          <div className='text-[#008146]'>Add Category</div>
+        ) : (
+          <>
+            <div className='text-[#008146]'>View Category</div>
+            <GeneralModal body={<EditGradeCategory />}>
+              <div className='text-[#008146]'>Edit Category</div>
+            </GeneralModal>
+          </>
+        )}
+      </div>
     ),
   },
 ];
 
 export default function GradeBookSettings() {
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const [data, setData] = useState([
+    {
+      type: 'ECCDE',
+      categories: [],
+    },
+    {
+      type: 'Primary',
+      categories: ['CA 1', 'CA 2', 'Examination'],
+    },
+    {
+      type: 'Secondary',
+      categories: [],
+    },
+    {
+      type: 'Tertiary',
+      categories: ['Test', 'Attendance', 'Examination'],
+    },
+  ]);
   return (
     <div className='flex flex-col gap-[22px] my-[22px]'>
       <div className='flex justify-end'>
@@ -23,49 +62,7 @@ export default function GradeBookSettings() {
         <div className='text-[#6B7A99] text-xl font-bold m-4'>
           Grade Category
         </div>
-        <ReactTable
-          columns={columns}
-          data={[
-            {
-              type: 'ECCDE',
-              category: '-',
-              actions: (
-                <>
-                  <div className='text-[#008146]'>Add Category</div>
-                </>
-              ),
-            },
-            {
-              type: 'Primary',
-              category: 'CA 1, CA 2, Examination',
-              actions: (
-                <>
-                  <div className='text-[#008146]'>View Category</div>
-                  <div className='text-[#008146]'>Edit Category</div>
-                </>
-              ),
-            },
-            {
-              type: 'Secondary',
-              category: '-',
-              actions: (
-                <>
-                  <div className='text-[#008146]'>Add Category</div>
-                </>
-              ),
-            },
-            {
-              type: 'Tertiary',
-              category: 'Test, Attendance, Examination',
-              actions: (
-                <>
-                  <div className='text-[#008146]'>View Category</div>
-                  <div className='text-[#008146]'>Edit Category</div>
-                </>
-              ),
-            },
-          ]}
-        ></ReactTable>
+        <ReactTable columns={columns} data={data}></ReactTable>
       </div>
     </div>
   );
