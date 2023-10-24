7115c7a9623bc80de7e7a9a8e9e3719eb0fc81ba
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2023-10-24 11:04:51 +0100

    feat: pdf to legacy

--- src/components/pdfReader/Reader.tsx
+++ src/components/pdfReader/Reader.tsx
@@ -7,7 +7,7 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import { pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 interface CustomPDFReaderProps {
   url: string
@@ -54,7 +54,7 @@ const CustomPDFReader = ({ url }: CustomPDFReaderProps) => {
 
   return (
     /*  Server Component */
-    <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js'>
+    <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.11.174/legacy/build/pdf.worker.js'>
       <div style={{ height: '100vh', width: '90vw' }}>
 
         <Viewer
