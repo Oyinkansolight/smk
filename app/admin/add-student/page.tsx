bb16c091069c44719ef025fc5a15eda4f0fc346d
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2023-11-17 17:03:37 +0100

    fix: page

--- app/admin/add-student/page.tsx
+++ app/admin/add-student/page.tsx
@@ -31,7 +31,7 @@ export default function AddStudent() {
     reValidateMode: 'onChange',
     mode: 'onChange',
   });
-  const [stage, setStage] = useState(3);
+  const [stage, setStage] = useState(1);
   const [imgSrc, setImgSrc] = useState(null);
   const [isOpen, setisOpen] = useState(false);
   const [loading, setloading] = useState(false);
